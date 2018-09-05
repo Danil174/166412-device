ae6f44a619832c3c37d90e66ecbbee150ea27951
Danil.M <milostiviy.danil@yandex.ru>
2018-09-05 22:44:46 +0500

    add modal show script

--- js/script.js
+++ js/script.js
@@ -1,36 +1,39 @@
-console.log("Hi!");
-// var loginList = document.querySelectorAll('.login');
-// console.log(loginList);
-var closeBtn = document.querySelector(".modal-close-button");
-closeBtn.addEventListener("click", function name(evt) {
+"use strict";
+
+var mapBtn = document.querySelector(".map-link");
+var closeBtns = document.querySelectorAll(".modal-close-button");
+var modalMap = document.querySelector(".modal-map");
+var mailBtn = document.querySelector(".mail-btn");
+var modalMail = document.querySelector(".modal-mailUs");
+
+mailBtn.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.remove("modal-show");
+  modalMail.classList.add("modal-show");
 });
 
-var popup = document.querySelector('.modal-login');
-var inputName = popup.querySelector('.modal-name');
-var login = document.querySelector('.login');
-console.log(login);
-login.addEventListener("click", function(evt) {
+mapBtn.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.toggle("modal-show");
-  inputName.focus();
+  modalMap.classList.add("modal-show");
 });
 
-var isStorageSupport = true;
-var storage = "";
+// не для ie
+// var closeBtns = Array.from(document.querySelectorAll(".modal-close-button"));
+// можно конечно полифил подключить для Array.from, но он больше всего моего скрипта
+// closeBtns.forEach(closeBtn => closeBtn.addEventListener("click", function (evt) {
+//   evt.preventDefault();
+//   this.parentElement.classList.remove("modal-show");
+// }));
 
-try {
-  storage = localStorage.getItem("inputName");
-} catch(err) {
-  isStorageSupport = false;
+var closeBtnsArr = [],
+  length = closeBtns.length;
+
+for (var i = 0; i < length; i++) {
+  closeBtnsArr.push(closeBtns[i]);
 }
 
-window.addEventListener("keydown", function (evt) {
-  if(evt.keyCode === 27) {
-    if (popup.classList.contains("modal-show")){
-      evt.preventDefault();
-      popup.classList.remove("modal-show");
-    }
-  }
+closeBtnsArr.forEach(function (closeBtn) {
+  return closeBtn.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    this.parentElement.classList.remove("modal-show");
+  });
 });
